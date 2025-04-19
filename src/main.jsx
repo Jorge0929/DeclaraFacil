a8826c854d93d096298115b0b08dd4ad0b66c249
Jorge0920 <jorgeandresabino123@gmail.com>
2025-04-18 22:48:54 -0500

    Cambiar importaciones incorrectas

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './assets/css/index.css'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
-        <App />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
   </StrictMode>,
 )
